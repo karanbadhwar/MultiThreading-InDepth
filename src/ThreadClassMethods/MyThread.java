1417927fa1b00b29a3fa319be677b4cdf473ad82
karanbadhwar <karanbadhwar07@gmail.com>
2024-08-13 10:54:41 -0400

    setDaemon() method

--- src/ThreadClassMethods/MyThread.java
+++ src/ThreadClassMethods/MyThread.java
@@ -4,18 +4,21 @@ public class MyThread extends Thread{
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " is running");
-            Thread.yield(); // Hint to the OS scheduler, to give chance to other Thread first!! HINT
+        int i = 0;
+        while(i < 10000)
+        {
+            System.out.println("Hello World");
+            i++;
         }
     }
 
+    //Daemon Threads are that runs in the Background, example Garbage Collector, and JVM do not wait for these Threads to finish
     public static void main(String[] args) throws InterruptedException {
+        //The Threads that created the Thread that runs the business logic and that do the heavy lifting is called User Thread.
         MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
-
+        t1.setDaemon(true); // This creates the User Thread to Daemon Thread, now Once the Main User Thread is finish, JVM will shut off the DaeMon Thread.
         t1.start();
-        t2.start();
+        System.out.println("End of Main Thread");
     }
 }
 
